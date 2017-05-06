76b167e97125dc93895e5545172a63d657468b16
aswinmohanme <aswinmmohan@gmail.com>
2017-05-06 20:58:49 +0530

    Setup Navigation

--- index.android.js
+++ index.android.js
@@ -1,44 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
 
-import {Examples} from '@shoutem/ui';
+import {Text} from '@shoutem/ui';
+import { StackNavigator } from 'react-navigation';
 
 export default class StarGaze extends Component {
   render() {
     return (
-      <Examples />
+      <Text> Hey </Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const App = StackNavigator({
+  HomeScreen: {screen: StarGaze},
 });
 
-AppRegistry.registerComponent('StarGaze', () => StarGaze);
+AppRegistry.registerComponent('StarGaze', () => App);
